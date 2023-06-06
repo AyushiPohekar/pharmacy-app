a5ede3e7cdc8fa29341319f94862a001e123b314
Ayushi Rhutwik Pohekar <ayushi2021.29@gmail.com>
2023-06-06 21:07:43 +0100

    Contact

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "./Sidebar.css";
-import DiamondIcon from '@mui/icons-material/Diamond';
+import DiamondIcon from "@mui/icons-material/Diamond";
 
 import { BiLogOutCircle } from "react-icons/bi";
 import {
@@ -8,12 +8,10 @@ import {
   MdCopyAll,
   MdContactSupport,
   MdCreateNewFolder,
-
 } from "react-icons/md";
 import { TbDiscount2 } from "react-icons/tb";
 
-import { AiOutlineBorderOuter } from "react-icons/ai";
-
+import { AiOutlineBorderOuter, AiOutlineClose } from "react-icons/ai";
 
 import { NavLink } from "react-router-dom";
 import Box from "@mui/material/Box";
@@ -23,45 +21,38 @@ import Modal from "@mui/material/Modal";
 import UpperSidebar from "./UpperSidebar";
 import SideNavLinks from "./SideNavLinks";
 
-
 //mayerial Ui modal style
 const style = {
-    position: "absolute",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
-    width: 400,
-    bgcolor: "background.paper",
-    border: "2px solid #000",
-    boxShadow: 24,
-    p: 4,
-  };
-
-
-
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: "620.72px",
+  background: "#FFFFFF",
+  border: "1px solid #CECECE",
+  borderRadius: "15px",
+  p: 0,
+  margin:0
+};
 
 const Sidebar = ({ children }) => {
   const [isOpen, setIsOpen] = useState(true);
   const toggle = () => setIsOpen(!isOpen);
- 
-      //material Ui Modal states
-      const [open, setOpen] = React.useState(false);
-      const handleOpen = () => setOpen(true);
-      const handleClose = () => setOpen(false);
 
+  //material Ui Modal states
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
-  
   return (
     <div className="Container">
       <div style={{ width: isOpen ? "275px" : "130.9px" }} className="sidebar">
         <div className="upperSidebar">
-        <UpperSidebar isOpen={isOpen} toggle={toggle}/>
-          
+          <UpperSidebar isOpen={isOpen} toggle={toggle} />
         </div>
         <div className="lowerSidebar">
           <div className="linkdiv">
-            <SideNavLinks isOpen={isOpen}/>
-            
+            <SideNavLinks isOpen={isOpen} />
           </div>
           <div
             className="Creatediv"
@@ -79,13 +70,15 @@ const Sidebar = ({ children }) => {
               aria-describedby="modal-modal-description"
             >
               <Box sx={style}>
-                <Typography id="modal-modal-title" variant="h6" component="h2">
-                  Text in a modal
-                </Typography>
-                <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-                  Duis mollis, est non commodo luctus, nisi erat porttitor
-                  ligula.
-                </Typography>
+                <div className="modalnav">
+                  <div className="modalnavtitle">Create New Order</div>
+                  <AiOutlineClose style={{color:"white",cursor:"pointer"}} onClick={handleClose}/>
+                  
+                </div>
+                <div className="ContactDiv">
+                    <div>Enter Customer Phone No</div>
+                    <input/>
+                  </div>
               </Box>
             </Modal>
           </div>
